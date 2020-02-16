0ba6c59f81d3e5d73bfe94349747251f701ed6dd
Aarush Gupta <aarushg94@gmail.com>
2020-02-16 18:46:16 -0500

    final push

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var sendMail = require('./routes/sendMail');
 
 var indexRouter = require('./routes/index');
 
-
 var app = express();
 
 // view engine setup
@@ -20,7 +19,7 @@ app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,21 +27,20 @@ app.use('/', indexRouter);
 app.use('/authorize', authorize);
 app.use('/send-mail', sendMail);
 
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
